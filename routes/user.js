e288927f1bc13f057512542138f4ab7d77de074f
Adeyemi Glory <adeyemiglr@gmail.com>
2022-05-08 16:45:54 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,33 @@ const router = require("express").Router();
 const Product = require("../modals/Product");
 const { cloudinary } = require("../cloudinary");
 
+// Post product
+// POST /api/user/post
+// public
+router.post("/user/post", async (req, res) => {
+  const { img1, img2, img3 } = req.body;
+  const image = async (img) => {
+    const uploadedResponse = await cloudinary.uploader.upload(img, {
+      upload_preset: "ml_default",
+    });
+    return uploadedResponse.url;
+  };
+
+  try {
+    const product = await Product.create({
+      ...req.body,
+      images: {
+        main: image(img1),
+        extra1: image(img2),
+        extra2: image(img3),
+      },
+    });
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // get single product
 // GET /api/product/:id
 // public
