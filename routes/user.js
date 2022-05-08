ba79515ac847ce5237740320bed9fa3ec0c6d64b
Adeyemi Glory <adeyemiglr@gmail.com>
2022-05-08 16:41:44 +0100

    get single product feature created

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,18 @@
 const router = require("express").Router();
+const Product = require("../modals/Product");
+const { cloudinary } = require("../cloudinary");
+
+// get single product
+// GET /api/product/:id
+// public
+router.get("/product/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
