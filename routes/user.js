ab7ae6d7160330826cbe280dff4ba15d6e9b80cf
Adeyemi Glory <adeyemiglr@gmail.com>
2022-05-08 16:43:50 +0100

    get all product feature created

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,16 @@
 const router = require("express").Router();
+const Product = require("../modals/Product");
+const { cloudinary } = require("../cloudinary");
+
+// get all product
+// GET /api/products
+// public
+router.get("/products", async (req, res) => {
+  try {
+    const all = await Product.find();
+    res.status(200).json(all);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
